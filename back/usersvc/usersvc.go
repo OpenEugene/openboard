09f7b5e11a167bb81db986ff01a7fb95d7e1c71f
Daved <daved@codemodus.com>
2019-02-26 12:24:40 -0800

    Add pb.userserver methods to usersvc

--- back/usersvc/usersvc.go
+++ back/usersvc/usersvc.go
@@ -1,9 +1,57 @@
 package usersvc
 
 import (
+	"context"
+
 	"github.com/champagneabuelo/openboard/back/pb"
 )
 
 var _ pb.UserServer = &UserSvc{}
 
-type UserSvc struct{}
+// UserSvc encapsulates dependencies and data required to implement the
+// pb.UserServer interface.
+type UserSvc struct {
+	// TODO: implement UserSvc
+}
+
+// AddRole implements part of the pb.UserServer interface.
+func (s *UserSvc) AddRole(ctx context.Context, req *pb.AddRoleReq) (*pb.RoleResp, error) {
+	// TODO: implement AddRole
+
+	return nil, nil
+}
+
+// FndRoles implements part of the pb.UserServer interface.
+func (s *UserSvc) FndRoles(ctx context.Context, req *pb.FndRolesReq) (*pb.RolesResp, error) {
+	// TODO: implement FndRoles
+
+	return nil, nil
+}
+
+// AddUser implements part of the pb.UserServer interface.
+func (s *UserSvc) AddUser(ctx context.Context, req *pb.AddUserReq) (*pb.UserResp, error) {
+	// TODO: implement AddUser
+
+	return nil, nil
+}
+
+// OvrUser implements part of the pb.UserServer interface.
+func (s *UserSvc) OvrUser(ctx context.Context, req *pb.OvrUserReq) (*pb.UserResp, error) {
+	// TODO: implement OvrUser
+
+	return nil, nil
+}
+
+// FndUsers implements part of the pb.UserServer interface.
+func (s *UserSvc) FndUsers(ctx context.Context, req *pb.FndUsersReq) (*pb.UsersResp, error) {
+	// TODO: implement FndUsers
+
+	return nil, nil
+}
+
+// RmvUser implements part of the pb.UserServer interface.
+func (s *UserSvc) RmvUser(ctx context.Context, req *pb.RmvUserReq) (*pb.RmvUserResp, error) {
+	// TODO: implement RmvUser
+
+	return nil, nil
+}
