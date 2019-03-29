ed454b13ed84448aa94012a9d9b51673ac7f4d33
Daved <daved@codemodus.com>
2019-03-28 19:28:47 -0700

    Add empty helloserver methods to helosvc

--- back/internal/helosvc/helosvc.go
+++ back/internal/helosvc/helosvc.go
@@ -1,6 +1,8 @@
 package helosvc
 
 import (
+	"context"
+
 	"github.com/champagneabuelo/openboard/back/internal/pb"
 )
 
@@ -15,9 +17,22 @@ func New() (*HeloSvc, error) {
 	return &HeloSvc{}, nil
 }
 
-/*type HelloServer interface {
-	AddHello(context.Context, *AddHelloReq) (*HelloResp, error)
-	OvrHello(context.Context, *OvrHelloReq) (*HelloResp, error)
-	RmvHello(context.Context, *RmvHelloReq) (*RmvHelloResp, error)
-	FndHellos(context.Context, *FndHellosReq) (*HellosResp, error)
-}*/
+// AddHello implements part of the pb.HelloServer interface.
+func (s *HeloSvc) AddHello(ctx context.Context, req *pb.AddHelloReq) (*pb.HelloResp, error) {
+	return nil, nil
+}
+
+// OvrHello implements part of the pb.HelloServer interface.
+func (s *HeloSvc) OvrHello(ctx context.Context, req *pb.OvrHelloReq) (*pb.HelloResp, error) {
+	return nil, nil
+}
+
+// RmvHello implements part of the pb.HelloServer interface.
+func (s *HeloSvc) RmvHello(ctx context.Context, req *pb.RmvHelloReq) (*pb.RmvHelloResp, error) {
+	return nil, nil
+}
+
+// FndHellos implements part of the pb.HelloServer interface.
+func (s *HeloSvc) FndHellos(ctx context.Context, req *pb.FndHellosReq) (*pb.HellosResp, error) {
+	return nil, nil
+}
