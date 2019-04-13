2dc19efe8357dc16012385484a7479d37225c3db
Daved <daved@codemodus.com>
2019-04-12 23:42:49 -0700

    Add tool versioning

---
+++ back/tools/tools.go
@@ -0,0 +1,16 @@
+// +build tools
+
+package tools
+
+//go:generate go install github.com/codemodus/withdraw
+//go:generate go install github.com/go-bindata/go-bindata/go-bindata
+//go:generate go install github.com/golang/protobuf/protoc-gen-go
+//go:generate go install github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway
+//go:generate go install github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger
+import (
+	_ "github.com/codemodus/withdraw"
+	_ "github.com/go-bindata/go-bindata/go-bindata"
+	_ "github.com/golang/protobuf/protoc-gen-go"
+	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway"
+	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger"
+)
