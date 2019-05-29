82175c8879be6bab4cb3b5a33c075cef00254a00
Greg McKelvey <mckelveygreg@gmail.com>
2019-05-28 20:36:59 -0700

    add title, textarea to new posts page

--- front/src/Page/Posts.elm
+++ front/src/Page/Posts.elm
@@ -1,6 +1,8 @@
 module Page.Posts exposing (Model, Msg(..), init, postsView, toSession, update, view)
 
-import Html.Styled exposing (text)
+import Html.Styled exposing (button, form, input, label, text, textarea)
+import Html.Styled.Attributes exposing (type_)
+import Html.Styled.Events exposing (onInput)
 import Route
 import Session exposing (Session)
 import Ui
@@ -9,23 +11,29 @@ import Ui
 type alias Model =
     { session : Session
     , slug : String
+    , title : String
+    , body : String
     }
 
 
 type Msg
-    = Noop
+    = SetTitle String
+    | SetBody String
 
 
 init : Session -> String -> ( Model, Cmd Msg )
 init session slug =
-    ( Model session slug, Cmd.none )
+    ( Model session slug "" "", Cmd.none )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Noop ->
-            ( model, Cmd.none )
+        SetTitle t ->
+            ( { model | title = t }, Cmd.none )
+
+        SetBody b ->
+            ( { model | body = b }, Cmd.none )
 
 
 view : Model -> { title : String, content : Html.Styled.Html Msg }
@@ -40,7 +48,17 @@ postsView model =
     Ui.card []
         [ Ui.heading "New Post"
         , Ui.linkBtn [ Route.href Route.Home ] [ text "Home" ]
-        , Ui.paragraph [] [ text "This is where new posts would go?" ]
+        , form []
+            [ label []
+                [ text "Title"
+                , input [ type_ "text", onInput SetTitle ] []
+                ]
+            , label []
+                [ text "Body"
+                , textarea [ onInput SetBody ] []
+                ]
+            , button [] [ text "submit" ]
+            ]
         ]
 
 
