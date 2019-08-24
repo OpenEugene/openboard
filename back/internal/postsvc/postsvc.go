749d92f138d0009165e1f2a8888d560b06ced565
codegold79 <codegold79@gmail.com>
2019-08-23 21:51:37 -0700

    implement grpcserver registerable with postsvc

--- back/internal/postsvc/postsvc.go
+++ back/internal/postsvc/postsvc.go
@@ -4,9 +4,12 @@ import (
 	"context"
 
 	"github.com/OpenEugene/openboard/back/internal/pb"
+	"google.golang.org/grpc"
 )
 
 var _ pb.PostServer = &PostSvc{}
+
+// var _ grpcsrv.Registerable = &PostSvc{}
 
 // PostSvc encapsulates dependencies and data required to implement the
 // pb.PostServer interface.
@@ -40,4 +43,10 @@ func (s *PostSvc) OvrPost(ctx context.Context, req *pb.OvrPostReq) (*pb.PostResp
 // RmvPost implements part of the pb.PostServer interface.
 func (s *PostSvc) RmvPost(ctx context.Context, req *pb.RmvPostReq) (*pb.RmvPostResp, error) {
 	return nil, nil
+}
+
+// RegisterWithGRPCServer implements the grpcsrv.Registerable interface.
+func (s *PostSvc) RegisterWithGRPCServer(g *grpc.Server) error {
+	pb.RegisterPostServer(g, s)
+	return nil
 }
