2596a1634225fb34fab96b1dd6abd7b233673b93
codegold79 <codegold79@gmail.com>
2019-08-23 21:57:00 -0700

    add relational db interface to postsvc

--- back/internal/postsvc/postsvc.go
+++ back/internal/postsvc/postsvc.go
@@ -11,9 +11,15 @@ var _ pb.PostServer = &PostSvc{}
 
 // var _ grpcsrv.Registerable = &PostSvc{}
 
+type relDB interface {
+	pb.PostServer
+}
+
 // PostSvc encapsulates dependencies and data required to implement the
 // pb.PostServer interface.
-type PostSvc struct{}
+type PostSvc struct {
+	db relDB
+}
 
 // New returns a pointer to a PostSvc instance or an error.
 func New() (*PostSvc, error) {
@@ -22,27 +28,27 @@ func New() (*PostSvc, error) {
 
 // AddType implements part of the pb.PostServer interface.
 func (s *PostSvc) AddType(ctx context.Context, req *pb.AddTypeReq) (*pb.TypeResp, error) {
-	return nil, nil
+	return s.db.AddType(ctx, req)
 }
 
 // AddPost implements part of the pb.PostServer interface.
 func (s *PostSvc) AddPost(ctx context.Context, req *pb.AddPostReq) (*pb.PostResp, error) {
-	return nil, nil
+	return s.db.AddPost(ctx, req)
 }
 
 // FndPosts implements part of the pb.PostServer interface.
 func (s *PostSvc) FndPosts(ctx context.Context, req *pb.FndPostsReq) (*pb.PostsResp, error) {
-	return nil, nil
+	return s.db.FndPosts(ctx, req)
 }
 
 // OvrPost implements part of the pb.PostServer interface.
 func (s *PostSvc) OvrPost(ctx context.Context, req *pb.OvrPostReq) (*pb.PostResp, error) {
-	return nil, nil
+	return s.db.OvrPost(ctx, req)
 }
 
 // RmvPost implements part of the pb.PostServer interface.
 func (s *PostSvc) RmvPost(ctx context.Context, req *pb.RmvPostReq) (*pb.RmvPostResp, error) {
-	return nil, nil
+	return s.db.RmvPost(ctx, req)
 }
 
 // RegisterWithGRPCServer implements the grpcsrv.Registerable interface.
