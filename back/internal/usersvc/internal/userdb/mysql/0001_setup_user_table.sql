812b063ec7c990bcc4a69cbf81d440f8259ce4ec
codegold79 <codegold79@gmail.com>
2019-09-11 22:19:24 -0700

    Switch back first,last_name to full_name

--- back/internal/usersvc/internal/userdb/mysql/0001_setup_user_table.sql
+++ back/internal/usersvc/internal/userdb/mysql/0001_setup_user_table.sql
@@ -11,8 +11,7 @@ CREATE TABLE user (
     email_hold BOOL NOT NULL DEFAULT TRUE,
     altmail VARCHAR(255) NOT NULL UNIQUE,
     altmail_hold BOOL NOT NULL DEFAULT TRUE,
-    first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL,
+    full_name VARCHAR(255) NOT NULL,
     avatar VARCHAR(255) NOT NULL DEFAULT "",
     password VARCHAR(255) NOT NULL,
 
