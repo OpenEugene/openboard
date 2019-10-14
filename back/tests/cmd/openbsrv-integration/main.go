40082e25db96500bf7fd35118823722d1353e529
codegold79 <codegold79@gmail.com>
2019-10-13 19:36:01 -0700

    Add relay lib

--- back/tests/cmd/openbsrv-integration/main.go
+++ back/tests/cmd/openbsrv-integration/main.go
@@ -4,26 +4,28 @@ import (
 	"context"
 	"fmt"
 	"github.com/OpenEugene/openboard/back/internal/pb"
+	"github.com/codemodus/relay"
 	"google.golang.org/grpc"
 )
 
 func main() {
-	var ctx context.Context
+	check, ff := relay.New().Fns()
+	defer func() { ff(recover()) }()
 
 	cc, err := grpc.Dial(":4242", grpc.WithInsecure())
-	if err != nil {
-		fmt.Println(err)
-	}
+	check(err)
+
 	defer cc.Close()
 
 	c := pb.NewUserSvcClient(cc)
 
-	r, err := c.AddRole(ctx, &pb.AddRoleReq{
-		Name: "testRole",
-	})
-	if err != nil {
-		fmt.Println(err)
-	}
-	
+	r, err := c.AddRole(
+		context.Background(),
+		&pb.AddRoleReq{
+			Name: "testRole",
+		},
+	)
+	check(err)
+
 	fmt.Printf("Response from server: %s", r)
 }
