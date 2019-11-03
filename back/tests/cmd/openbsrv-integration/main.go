3ccd939a69a926b07fb52396b841a265ff607503
codegold79 <codegold79@gmail.com>
2019-11-03 13:49:28 -0800

    Add tests for AddUser, FndUsers, RemUser, OvrUser

--- back/tests/cmd/openbsrv-integration/main.go
+++ back/tests/cmd/openbsrv-integration/main.go
@@ -38,7 +38,7 @@ func userServiceTests() {
 			Username:    "test user name a",
 			Email:       "test user email a",
 			EmailHold:   false,
-			Altmail:     "",
+			Altmail:     "test alt email a",
 			AltmailHold: false,
 			FullName:    "test user full name a",
 			Avatar:      "test user avatar a",
@@ -46,7 +46,75 @@ func userServiceTests() {
 		},
 	)
 	check(err)
-	fmt.Printf("Response from user service add user: %s\n", r2)
+	fmt.Printf("Response from user service add user A: %s\n", r2)
+
+	r3, err := c.AddUser(
+		context.Background(),
+		&pb.AddUserReq{
+			Username:    "test user name b",
+			Email:       "test user email b",
+			EmailHold:   false,
+			Altmail:     "test alt email b",
+			AltmailHold: false,
+			FullName:    "test user full name b",
+			Avatar:      "test user avatar b",
+			Password:    "test user password b",
+		},
+	)
+	check(err)
+	fmt.Printf("Response from user service add user B: %s\n", r3)
+
+	r4, err := c.FndUsers(
+		context.Background(),
+		&pb.FndUsersReq{
+			Email:       "test user email b",
+			EmailHold:   false,
+			Altmail:     "test alt email b",
+			AltmailHold: false,
+		},
+	)
+	check(err)
+	fmt.Printf("Response from user service find user B: %s\n", r4)
+
+	// Delete the user_id for user B
+	r5, err := c.RmvUser(
+		context.Background(),
+		&pb.RmvUserReq{
+			Id: r4.Items[0].Id,
+		},
+	)
+	check(err)
+	fmt.Printf("Response from user service remove user B: %s\n", r5)
+
+	r6, err := c.FndUsers(
+		context.Background(),
+		&pb.FndUsersReq{
+			Email:       "test user email a",
+			EmailHold:   false,
+			Altmail:     "",
+			AltmailHold: false,
+		},
+	)
+	check(err)
+	fmt.Printf("Response from user service find user A: %s\n", r6)
+
+	// Update existing user A username
+	r7, err := c.OvrUser(
+		context.Background(),
+		&pb.OvrUserReq{
+			Id: r6.Items[0].Id,
+			Req: &pb.AddUserReq{
+				Username:    "test username Aye",
+				Email:       "test user email a",
+				EmailHold:   false,
+				Altmail:     "test user alt_mail a",
+				AltmailHold: false,
+				Avatar:      "test user a avatar",
+			},
+		},
+	)
+	check(err)
+	fmt.Printf("Response from user service remove user B: %s\n", r7)
 }
 
 func postServiceTests() {
