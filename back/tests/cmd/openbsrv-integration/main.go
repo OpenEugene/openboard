06fa8f74e278352e4fcd86d40f3c3cefb0c530aa
codegold79 <codegold79@gmail.com>
2019-12-07 10:41:47 -0800

    Update user service tests for users and roles
    Add user B, find non-existant user C, add role 2

--- back/tests/cmd/openbsrv-integration/main.go
+++ back/tests/cmd/openbsrv-integration/main.go
@@ -26,46 +26,73 @@ func main() {
 }
 
 func userSvcAddRoles(conn *grpc.ClientConn, clnt pb.UserSvcClient) {
-	r, err := clnt.AddRole(
+	r1, err := clnt.AddRole(
 		context.Background(),
 		&pb.AddRoleReq{
-			Name: "testRole",
+			Name: "testRole 1",
 		},
 	)
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	fmt.Printf("Response from user service add role:\n%s\n\n", r)
+	r2, err := clnt.AddRole(
+		context.Background(),
+		&pb.AddRoleReq{
+			Name: "testRole 2",
+		},
+	)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Printf("Response from user service added roles:\n%s\n%s\n\n", r1, r2)
 }
 
 func userSvcAddUsers(conn *grpc.ClientConn, clnt pb.UserSvcClient) {
-	r, err := clnt.AddUser(
+	r1, err := clnt.AddUser(
 		context.Background(),
 		&pb.AddUserReq{
-			Username:    "test username a",
-			Email:       "test user email a",
+			Username:    "test username A",
+			Email:       "user_a@email.com",
 			EmailHold:   false,
 			Altmail:     "",
 			AltmailHold: false,
-			FullName:    "test user full name a",
-			Avatar:      "test user avatar a",
-			Password:    "test user password a",
+			FullName:    "test user full name A",
+			Avatar:      "test user avatar A",
+			Password:    "test user password A",
 		},
 	)
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	fmt.Printf("Response from user service add user:\n%s\n\n", r.Item)
+	r2, err := clnt.AddUser(
+		context.Background(),
+		&pb.AddUserReq{
+			Username:    "test username B",
+			Email:       "userB@email.com",
+			EmailHold:   false,
+			Altmail:     "",
+			AltmailHold: false,
+			FullName:    "test user full name B",
+			Avatar:      "test user avatar B",
+			Password:    "test user password B",
+		},
+	)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Printf("Response from user service add user A and user B:\n%s\n%s\n\n", r1.Item, r2.Item)
 }
 
 func userSvcFndUsers(conn *grpc.ClientConn, clnt pb.UserSvcClient) {
-	r, err := clnt.FndUsers(
+	r1, err := clnt.FndUsers(
 		context.Background(),
 		&pb.FndUsersReq{
 			RoleIds:     []string{},
-			Email:       "test user email a",
+			Email:       "userB@email.com",
 			EmailHold:   false,
 			Altmail:     "",
 			AltmailHold: false,
@@ -77,7 +104,23 @@ func userSvcFndUsers(conn *grpc.ClientConn, clnt pb.UserSvcClient) {
 		fmt.Println(err)
 	}
 
-	fmt.Printf("Response from user service find user a:\n%s\n\n", r)
+	r2, err := clnt.FndUsers(
+		context.Background(),
+		&pb.FndUsersReq{
+			RoleIds:     []string{},
+			Email:       "userC@email.com",
+			EmailHold:   false,
+			Altmail:     "",
+			AltmailHold: false,
+			Limit:       100,
+			Lapse:       0,
+		},
+	)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Printf("Response from user service find one existing user and one not:\n%s\n%s\n\n", r1, r2)
 }
 
 func postSvcAddPosts(conn *grpc.ClientConn, clnt pb.PostClient) {
