f740e31fa1529d76b6134a8dd9d2bca4b6eb5778
daved <daved@codemodus.com>
2021-06-01 18:34:51 -0700

    Rearrange proto dep locations

--- back/internal/httpsrv/gogen.go
+++ back/internal/httpsrv/gogen.go
@@ -1,7 +1,7 @@
 package httpsrv
 
 // alias protobuf/swagger
-//go:generate -command pb-sw protoc -I ../../../msgs/proto -I ../../../msgs/proto/vendor-extra/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis --openapiv2_out=logtostderr=true,allow_merge=true:.
+//go:generate -command pb-sw protoc -I ../../../msgs/proto -I ../../tools/.include -I ../../../msgs/proto/include/googleapis --openapiv2_out=logtostderr=true,allow_merge=true:.
 
 // generate swagger.json
 //go:generate pb-sw ../../../msgs/proto/auth.proto ../../../msgs/proto/user.proto ../../../msgs/proto/post.proto
