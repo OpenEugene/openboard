5338a57be3c699553377e4c76f28973e8cbd288d
codegold79 <codegold79@gmail.com>
2021-07-10 10:56:56 -0700

    Add benchmark test
    Signed-off-by: codegold79 <codegold79@gmail.com>

---
+++ back/internal/dbg/dbg_test.go
@@ -0,0 +1,22 @@
+package dbg
+
+import (
+	"os"
+	"testing"
+)
+
+func BenchmarkDbgUse(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		useDbgLogExample()
+	}
+}
+
+func useDbgLogExample() {
+	Log("\ndebug is writing to nothing")
+
+	SetDebugOut(os.Stdout)
+
+	Log("debug has been set to write to stdout")
+
+	Logf("writing various formats: %t, %s, %d", true, "word", 79)
+}
